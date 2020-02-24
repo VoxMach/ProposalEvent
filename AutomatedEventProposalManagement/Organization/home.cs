8a08d3b15102e456b1c914c86db12eecc726c4d9
Arbiterchil <arbiterchil@gmail.com>
2020-02-24 23:14:54 +0800

    done me

--- AutomatedEventProposalManagement/Organization/home.cs
+++ AutomatedEventProposalManagement/Organization/home.cs
@@ -7,11 +7,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
 
 namespace AutomatedEventProposalManagement
 {
     public partial class home : Form
     {
+        IFirebaseConfig config = new FirebaseConfig
+        {
+            AuthSecret = "20O72YtkHWmSZDIZ2kF01hmg2T3iSetxJO58CuIu",
+            BasePath = "https://event-proposal.firebaseio.com/"
+        };
+        IFirebaseClient client;
         public home()
         {
             InitializeComponent();
@@ -20,16 +29,116 @@ namespace AutomatedEventProposalManagement
 
         private void home_Load(object sender, EventArgs e)
         {
-            
-            this.CenterToScreen();
-            this.Size = Screen.PrimaryScreen.WorkingArea.Size;
-            this.WindowState = FormWindowState.Normal;
+            try
+            {
+                client = new FireSharp.FirebaseClient(config);
+                if (client != null)
+                {
+
+                    this.CenterToScreen();
+                    this.Size = Screen.PrimaryScreen.WorkingArea.Size;
+                    this.WindowState = FormWindowState.Normal;
+
+
+                }
+            }
+            catch
+            {
+                MessageBox.Show("No Internet or Connection Problem");
+            }
 
             nameu.Text = loginForm.s1 +","+loginForm.s5 +" "+loginForm.s6;
             label2.Text = loginForm.s2;
             label3.Text = loginForm.s3;
             label4.Text = loginForm.s4;
- 
+
+            string today = DateTime.Now.ToString("yyyy-MM-dd");
+            var today1 = DateTime.Now;
+            var tomorrow = today1.AddDays(1);
+
+            FirebaseResponse response = client.Get("SAO/Proposal/");
+
+            Dictionary<string, propose> Dick = response.ResultAs<Dictionary<string, propose>>();
+            foreach (var find in Dick)
+            {
+
+                string datnow = find.Value.date_of_event;
+
+                if (today.Equals(datnow))
+                {
+                    if (find.Value.status.Equals("Accepted"))
+                    {
+
+                        dataGridView1.Update();
+                        dataGridView1.Refresh();
+                        dataGridView1.Rows.Add(
+                    find.Value.beneficiaries,
+                    find.Value.committee_in_charge,
+                    find.Value.date,
+                    find.Value.date_of_event,
+                    find.Value.description,
+                   find.Value.id,
+                    find.Value.name_of_project,
+                    find.Value.nature_of_project,
+                    find.Value.noted_by_adviser,
+                    find.Value.noted_by_org_president,
+                    find.Value.org_name,
+                    find.Value.org_type,
+                    find.Value.prepared_by,
+                    find.Value.recommending_approval,
+                    find.Value.status,
+                    find.Value.time_from,
+                    find.Value.time_to,
+                    find.Value.venue
+                            );
+
+                    }
+                }
+
+
+
+            }
+            FirebaseResponse response1 = client.Get("SAO/Proposal/");
+
+            Dictionary<string, propose> Dick1 = response1.ResultAs<Dictionary<string, propose>>();
+            foreach (var find in Dick1)
+            {
+
+                string datnow = find.Value.date_of_event;
+
+                if (tomorrow.Equals(tomorrow))
+                {
+
+
+                    dataGridView2.Update();
+                    dataGridView2.Refresh();
+                    dataGridView2.Rows.Add(
+                find.Value.beneficiaries,
+                find.Value.committee_in_charge,
+                find.Value.date,
+                find.Value.date_of_event,
+                find.Value.description,
+               find.Value.id,
+                find.Value.name_of_project,
+                find.Value.nature_of_project,
+                find.Value.noted_by_adviser,
+                find.Value.noted_by_org_president,
+                find.Value.org_name,
+                find.Value.org_type,
+                find.Value.prepared_by,
+                find.Value.recommending_approval,
+                find.Value.status,
+                find.Value.time_from,
+                find.Value.time_to,
+                find.Value.venue
+                        );
+
+                    break;
+                }
+            }
+
+
+
         }
 
         private void panel2_Paint(object sender, PaintEventArgs e)
