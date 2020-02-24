6bbd9c0211d5d9506d0501c4dc0558cb5a6bb69d
Zhai <jaycemico@gmail.com>
2020-02-24 10:01:28 +0800

    asdasda

--- AutomatedEventProposalManagement/Approver/apphome.Designer.cs
+++ AutomatedEventProposalManagement/Approver/apphome.Designer.cs
@@ -120,31 +120,31 @@
             this.nameu.ForeColor = System.Drawing.Color.White;
             this.nameu.Location = new System.Drawing.Point(29, 18);
             this.nameu.Name = "nameu";
-            this.nameu.Size = new System.Drawing.Size(72, 23);
+            this.nameu.Size = new System.Drawing.Size(86, 30);
             this.nameu.TabIndex = 3;
-            this.nameu.Text = "label2";
+            this.nameu.Text = "name";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(277, 73);
+            this.label3.Location = new System.Drawing.Point(312, 91);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(72, 23);
+            this.label3.Size = new System.Drawing.Size(86, 30);
             this.label3.TabIndex = 5;
-            this.label3.Text = "label3";
+            this.label3.Text = "org name";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(277, 18);
+            this.label2.Location = new System.Drawing.Point(312, 23);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(72, 23);
+            this.label2.Size = new System.Drawing.Size(86, 30);
             this.label2.TabIndex = 4;
-            this.label2.Text = "label2";
+            this.label2.Text = "id";
             // 
             // panel2
             // 
@@ -173,6 +173,17 @@
             this.label5.TabIndex = 7;
             this.label5.Text = "label5";
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(671, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(220, 30);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "organization type";
+            // 
             // label4
             // 
             this.label4.AutoSize = true;
@@ -180,9 +191,20 @@
             this.label4.ForeColor = System.Drawing.Color.White;
             this.label4.Location = new System.Drawing.Point(29, 73);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(72, 23);
+            this.label4.Size = new System.Drawing.Size(86, 30);
             this.label4.TabIndex = 6;
             this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(564, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 30);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "label5";
             // 
             // apphome
             // 
